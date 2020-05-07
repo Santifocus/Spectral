862c042c82938ae42f782a51698812bc1b1f6362
Lucas <saculucas98@aol.com>
2020-05-07 08:42:28 +0200

    Resolution Setter for Build + PP Changes

---
+++ Assets/Scripts/Runtime/ResolutionSetter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionSetter : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.SetResolution(Mathf.RoundToInt(Screen.height * (9f / 16f)), Screen.height, true);
+    }
+}
